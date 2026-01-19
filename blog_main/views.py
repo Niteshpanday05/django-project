5f395acf5ecf70a253f7a7ad937e4d9e745a07ea
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-19 19:51:12 +0545

    adding dashboard

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
