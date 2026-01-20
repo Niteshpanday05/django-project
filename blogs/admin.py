a55262d59133dfa1c9fb751b453fbcddf374ab9c
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-20 12:07:36 +0545

    blog was created

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from . models import Category
+from . models import Category,Blog
+
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category','author', 'status', 'is_featured')
+    search_fields=('id', 'title','category__category__name', 'status')
+    
 
 # Register your models here.
 
 admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
