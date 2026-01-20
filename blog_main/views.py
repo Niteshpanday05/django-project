ac0ce056ab3b893fe22c3f943ae5329cc6c07ff7
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-20 17:45:21 +0545

    adding some style

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Blog, Category
 
 
 
 def home(request):
     categories=Category.objects.all()
+    featured_posts=Blog.objects.filter(is_featured=True).order_by('-updated_at')
+    
     context = {
         'categories' : categories,
+        'featured_posts' : featured_posts,
     }
-    return render(request,'home.html',{'categories':categories})
+    return render(request,'home.html', context)
 
 def search(request):
     return render(request, 'search.html')
