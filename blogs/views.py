1f3d208d36e69ea4b363e3306a52fb5a5aea134e
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-21 20:07:32 +0545

    404 error pag done

--- blogs/views.py
+++ blogs/views.py
@@ -10,13 +10,13 @@ def posts_by_category(request,category_id):
     #fetch the post that belongs to the category with the id category_id
     posts=Blog.objects.filter(status='Published', category=category_id)
    
-    try:
-         category= Category.objects.get(pk=category_id)
-    except:
-        # redirect to home page
-        return redirect('home')
+    # try:
+    #      category= Category.objects.get(pk=category_id)
+    # except:
+    #     # redirect to home page
+    #     return redirect('home')
     
-    # category = get_object_or_404(Category, pk=category_id)
+    category = get_object_or_404(Category, pk=category_id)
     
     
     context = {
