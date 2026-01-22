2199c61076ad5d4da27c938fab4eccf2a86b416a
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-22 21:02:26 +0545

    error solved

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$$5a$k8wb1&h8depac4p7-!qs(*uv=!thn9!l2!x6onwln@^0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
